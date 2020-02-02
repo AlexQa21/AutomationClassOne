07b567d70a0685ea0a024198a2f88f68677fe7e6
adily <adily@192.168.86.36>
2020-02-02 16:46:53 -0500

    q

---
+++ AutomationClassOne/src/main/java/com/DataTypePackage/DataType.java
@@ -0,0 +1,49 @@
+package com.DataTypePackage;
+
+public class DataType {
+
+	
+//	This is another line comment
+	
+	/*This 
+	is 
+    multiline
+	comment*/
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int x = 1;
+		int y = 2;
+		int j = 3;
+		x = 5;
+        // double datatype
+		double a = 1.5;
+		 a = 2.1;
+		 
+		 //character datatype
+		 char z = 'z';
+		 //String is a non primitive data type. String its a class.
+		 
+		 String name = "Khan";
+		 
+		 //Boolean Datatype
+		 
+		 boolean b = true;
+		 boolean c = false;
+		 
+		 
+		 
+		System.out.println("My name is " + name);
+		 
+		 
+	}
+	
+	
+	public static void Addition()  {
+		
+	}
+
+}
